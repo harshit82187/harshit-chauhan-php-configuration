46bfb7d6da8670360a769a55568a7f8f4dd6ae1a
harshit82187 <150501136+harshit82187@users.noreply.github.com>
2024-08-28 18:42:53 +0530

    Update Middleware In Laravel.php

--- Middleware In Laravel.php
+++ Middleware In Laravel.php
@@ -4,6 +4,39 @@
 php artisan make:middleware AdminMiddleware
 
 
+/////////////////////////////// config>auth ////////////////////////////////////////////////////
+
+'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+    ],
+
+
+
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
+
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
+
+        // 'users' => [
+        //     'driver' => 'database',
+        //     'table' => 'users',
+        // ],
+    ],
+
 
 ////////////////////////////// app\Http\Middleware\AdminMiddleware //////////////////////////////
 <?php
@@ -18,15 +51,16 @@ class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role == 121) {
-            // User is authenticated and has the required role
-            return $next($request);
+        // dd('Middleware executed');
+        // Check if the user is authenticated and is an admin
+        if (!Auth::guard('admin')->check()) {
+            return redirect()->route('admin.login')->with('error','Unautorized user!'); // Redirect to admin login if not authenticated
         }
 
-        // Redirect to login page or show an error message
-        return redirect()->route('admin.login')->with('error', 'Unauthorized access');
+        return $next($request);
     }
 }
+
 
 
 
@@ -53,24 +87,67 @@ Route::get('/clear', function () {
 });
 
 
+Route::match(['get','post'], 'admin/login', [AuthController::class, 'login'])->name('admin.login');
 
-Route::prefix('admin')->name('admin.')->group(function (){
-  Route::match(['get','post'], 'login', [AuthController::class, 'login'])->name('login');
+Route::prefix('admin/')->name('admin.')->middleware('admin')->group(function(){
+        Route::controller(AuthController::class)->group(function(){
+            Route::get('dashboard', 'dashboard')->name('dashboard');
+            Route::get('users', 'users')->name('users');
 
-  Route::middleware('admin')->group(function (){
-      Route::controller(AuthController::class)->group(function(){
-          Route::get('dashboard', 'dashboard')->name('dashboard');
+            Route::get('logout', 'logout')->name('logout');
 
-          Route::get('logout', 'logout')->name('logout');        
-
-      });
-     
-  });
-
+        });
 });
 
 
 
+public function logout(){
+    Auth::guard('admin')->logout();
+    session()->flush();
+    // dd('Logged out');
+    return redirect()->route('admin.login')->with('error','Logout Successfully!');
+}
+
+
+public function login(Request $req){
+
+    try{
+        if($req->isMethod('get')){
+            // dd(121);
+            // dd(Auth::guard('admin')->check());
+            return view('backend.auth.login');
+
+        }else{
+
+            $req->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            $email = $req->email;
+            $password = $req->password;
+
+            $admin = Admin::where('email',$email)->first();
+            if($admin){
+                if(Auth::guard('admin')->attempt([ 'email' => $email, 'password' => $password ])){
+                    return redirect()->route('admin.dashboard')->with('success','Login Successfully!');
+                }else{
+                    return back()->with('error','Wrong Credentials!');
+                }    
+            }else{
+                return back()->with('error','Record Not Found!');
+            }   
+            
+        }
+
+    }catch (ValidationException $e) {
+        return back()->withErrors($e->validator)->withInput();
+    }catch(\Exception $e){
+        return back()->with('error', 'Warning : ' .$e->getMessage());
+    }
+
+    
+}
 
 
 
