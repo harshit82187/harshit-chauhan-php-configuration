8a3af53a96806e4dc3a6a110fdd1bf3a0ec344b7
harshit82187 <150501136+harshit82187@users.noreply.github.com>
2024-09-26 13:40:47 +0530

    Update Whatsapp Api.php

--- Whatsapp Api.php
+++ Whatsapp Api.php
@@ -42,3 +42,103 @@ public function sendWhatsappMessage(Request $req){
         }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+************************************************** When you senf pdf or image with message *****************************************************************************
+ public function sendWhatsappMessage(Request $req){
+        // dd($req->all());
+
+        $req->validate([
+            'college_id'   => 'required|numeric',
+            'student_id'   => 'required|string',
+            'user_id'      => 'required|numeric',
+            'student_name' => 'required|string',
+            'student_mobile_no' => 'required|string',
+            'message' => 'required|string',
+            'attachment' => 'file|max:2048',
+
+        ]);
+
+        $studentName = $req->input('student_name');
+        $studentMobileNo = $req->input('student_mobile_no');
+        $message = "Hello " . $studentName . ",\n" . $req->input('message');
+      
+
+        if (!str_starts_with($studentMobileNo, '+91')) {
+            $studentMobileNo = '+91' . $studentMobileNo;
+        }
+        $apiKey = 'EfJ3kJdXG6cz'; 
+        $whatsappApiUrl = 'http://api.textmebot.com/send.php';
+
+        $data = [
+            'user_id' => $req->user_id,
+            'student_id' => $req->student_id,
+            'college_id' => $req->college_id,
+            'message'    => $req->message,
+        ];
+
+        if($req->hasFile('attachment')){
+            $file = $req->file('attachment');
+            $extension = $file->getClientOriginalExtension();
+            $filename = uniqid() . '.' . $file->getClientOriginalExtension();
+            $filePath = public_path('college-conversation/' . $filename);
+            $file->move(public_path('college-conversation'),$filename);
+            // dd($filePath);
+            $data['attachment'] = $filename;
+            
+
+            if ($extension === 'pdf') {
+
+                $response = Http::get($whatsappApiUrl, [
+                    'recipient' => $studentMobileNo,
+                    'apikey' => $apiKey,
+                    'text' => $message,
+                    'document' => $filePath,
+                ]);
+
+            }elseif (in_array($extension, ['jpg', 'jpeg', 'png'])) {
+                $response = Http::get($whatsappApiUrl, [
+                    'recipient' => $studentMobileNo,
+                    'apikey' => $apiKey,
+                    'text' => $message,
+                    'file' => $filePath,
+                ]);
+               
+            }
+        }
+
+        $response = Http::get($whatsappApiUrl, [
+            'recipient' => $studentMobileNo,
+            'apikey' => $apiKey,
+            'text' => $message,
+        ]);
+
+        WhatsappConversation::create($data);
+        
+
+        if ($response->successful()) {
+            return response()->json([
+                'message' => 'WhatsApp message sent successfully!'
+            ], 200);
+        } else {
+            return response()->json([
+                'errors' => ['message' => 'Failed to send WhatsApp message.']
+            ], 500);
+        }
+
+    }
