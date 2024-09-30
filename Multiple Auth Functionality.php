38601bf5931d351a21ea99c400a97b3a977c12b3
harshit82187 <150501136+harshit82187@users.noreply.github.com>
2024-09-30 09:22:46 +0530

    Update Multiple Auth Functionality.php

--- Multiple Auth Functionality.php
+++ Multiple Auth Functionality.php
@@ -247,6 +247,37 @@ return [
 
 
 
+*********************************** app/http/kernel.php *************************************************************************
+
+
+
+  protected $middlewareAliases = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \App\Http\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'superadmin'    => \App\Http\Middleware\SuperAdminMiddleware::class,
+        'admin'    => \App\Http\Middleware\AdminMiddleware::class,
+        'collegeadmin'    => \App\Http\Middleware\CollegeAdminMiddleware::class,
+        'counsellor'    => \App\Http\Middleware\CounsellorMiddleware::class,
+
+    ];
+    
+
+
+
+    
+
+
+
+
+
 *********************************** Admin Middleware Code *****************************************************************
 
 
