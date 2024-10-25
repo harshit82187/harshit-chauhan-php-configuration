b0020ef295cd0f6a22672a1c59a1d83ea500dd46
harshit82187 <150501136+harshit82187@users.noreply.github.com>
2024-10-25 12:29:37 +0530

    Update Popup message with iziToast And Summernote.php

--- Popup message with iziToast And Summernote.php
+++ Popup message with iziToast And Summernote.php
@@ -66,25 +66,40 @@ catch (ValidationException $e) {
 
 
 *********************************************** Another Method ********************************************************************************
-@if(session()->get('success'))
-    <div class="alert alert-success" role="alert">
-        {{ session()->get('success')}}
-    </div>
-    @elseif(session()->get('error'))
-        <div class="alert alert-danger" role="alert">
-            {{ session()->get('error')}}
-        </div>
-@endif
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+<div class="row">
+    <div class="col-12">
+        @if(session()->get('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session()->get('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @elseif(session()->get('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session()->get('error') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+
+        @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li style="font-size:larger;">{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
     </div>
-@endif
+</div>
+
 
 
 
